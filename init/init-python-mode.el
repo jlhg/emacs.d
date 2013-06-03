00be87ccc75ae4b8f2267764899d2b4298968b47
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-06-04 00:30:00 +0800

    Set comment-inline-offset to 2 in python-mode

--- init/init-python-mode.el
+++ init/init-python-mode.el
@@ -3,4 +3,8 @@
           '(lambda ()
              (setq python-check-command "python-check.sh")
              (define-key python-mode-map (kbd "C-c w") 'python-check)))
+
+(add-hook 'python-mode-hook
+   (lambda () (set (make-local-variable 'comment-inline-offset) 2)))
+
 (provide 'init-python-mode)
