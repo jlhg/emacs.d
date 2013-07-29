c1b387ce2edd1be47c2bb856419f7643f54bc76b
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-07-29 15:15:23 +0800

    Fix c-mode

--- init/init-c-mode.el
+++ init/init-c-mode.el
@@ -1,7 +1,6 @@
+(setq c-default-style "linux")
 (add-hook 'c-mode-hook
           '(lambda ()
-             (setq c-default-style "linux"
-                   c-basic-offset 4)
              (setq ac-sources (delq 'ac-source-yasnippet ac-sources))))
 
 (provide 'init-c-mode)
