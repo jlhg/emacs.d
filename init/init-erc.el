7680e9e3d03c36436ea9c8394ca1eefb1ab3b88b
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-11-23 23:58:30 +0800

    Fix erc

--- init/init-erc.el
+++ init/init-erc.el
@@ -1,7 +1,8 @@
 ;; highlight nicknames
-(require 'erc-highlight-nicknames)
-(add-to-list 'erc-modules 'highlight-nicknames)
-(erc-update-modules)
+(and
+ (require 'erc-highlight-nicknames)
+ (add-to-list 'erc-modules 'highlight-nicknames)
+ (erc-update-modules))
 
 ;; erc: hide join part messages
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
