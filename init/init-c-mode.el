ddeb19f008ab43fe0738d51b72fed95bf002e0b7
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-12-30 01:49:36 +0800

    c-mode: Revert to the last commit

--- init/init-c-mode.el
+++ init/init-c-mode.el
@@ -1,7 +1,6 @@
 (setq c-default-style "linux")
 (add-hook 'c-mode-hook
           '(lambda ()
-             (setq ac-sources (delq 'ac-source-yasnippet ac-sources))
-             (setq comment-style 'multi-line)))
+             (setq ac-sources (delq 'ac-source-yasnippet ac-sources))))
 
 (provide 'init-c-mode)
