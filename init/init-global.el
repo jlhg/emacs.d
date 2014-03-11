6121d027ce24200c3bb94ba74689374966cf870f
Jian-Long Huang <jlh@pyhub.org>
2014-03-12 01:35:07 +0800

    Change the theme to Zenburn

--- init/init-global.el
+++ init/init-global.el
@@ -1,17 +1,22 @@
 ;; color theme
-(add-to-list 'load-path "~/.emacs.d/package/color-theme")
-(require 'color-theme)
-(require 'color-theme-customized)
-(color-theme-initialize)
-(color-theme-customized)
+;; (add-to-list 'load-path "~/.emacs.d/package/color-theme")
+;; (require 'color-theme)
+;; (require 'color-theme-customized)
+;; (color-theme-initialize)
+;; (color-theme-customized)
 ;; (color-theme-select)
 ;; (color-theme-comidia)
 
 ;; customized faces for python mode
-(font-lock-add-keywords 'python-mode
-                        '(;;("\\<\\(object\\|str\\|else\\|except\\|finally\\|try\\|\\)\\>" 0 'py-builtins-face)  ; adds object and str and fixes it so that keywords that often appear with : are assigned as builtin-face
-                          ;; ("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?\\>" 0 'font-lock-defaults) ; FIXME: negative or positive prefixes do not highlight to this regexp but does to one below
-                          ("\\([][{}()~^<>:=,.\\+*/%-]\\)" 0 'font-lock-type-face)))
+;; (font-lock-add-keywords 'python-mode
+;;                         '(;;("\\<\\(object\\|str\\|else\\|except\\|finally\\|try\\|\\)\\>" 0 'py-builtins-face)  ; adds object and str and fixes it so that keywords that often appear with : are assigned as builtin-face
+;;                           ;; ("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?\\>" 0 'font-lock-defaults) ; FIXME: negative or positive prefixes do not highlight to this regexp but does to one below
+;;                           ("\\([][{}()~^<>:=,.\\+*/%-]\\)" 0 'font-lock-type-face)))
+
+;; Zenburn-theme
+;; https://github.com/bbatsov/zenburn-emacs
+(require 'zenburn-theme)
+(load-theme 'zenburn t)
 
 ;; starting message
 (setq initial-scratch-message "")
