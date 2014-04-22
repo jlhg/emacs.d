175a70727903a4e974f3c7624f9058777273453e
Jian-Long Huang <jlh@pyhub.org>
2014-04-22 14:23:48 +0800

    Remove erc-highlight-nicknames.el

--- init/init-erc.el
+++ init/init-erc.el
@@ -1,9 +1,3 @@
-;; highlight nicknames
-(and
- (require 'erc-highlight-nicknames)
- (add-to-list 'erc-modules 'highlight-nicknames)
- (erc-update-modules))
-
 ;; erc: hide join part messages
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
