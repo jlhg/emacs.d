10e3f6caa5ec41a359ffd7e613712c00a58eb148
Jian-Long Huang <jlh@pyhub.org>
2014-05-09 15:55:57 +0800

    Fix the indentation offset in sh-mode

--- init/init-sh-mode.el
+++ init/init-sh-mode.el
@@ -1,6 +1,7 @@
 ;; indentation: 2 spaces
 (add-hook 'sh-mode-hook
           (lambda ()
-            (setq sh-basic-offset 2)))
+            (setq sh-basic-offset 2)
+            (setq sh-indentation 2)))
 
 (provide 'init-sh-mode)
