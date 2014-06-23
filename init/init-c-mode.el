8b95bb1b6130c17f40338b7ac05050516ba9d1ff
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-06-23 19:05:16 +0800

    c-mode: use kernel's comment style

--- init/init-c-mode.el
+++ init/init-c-mode.el
@@ -1,4 +1,5 @@
 (setq c-default-style "linux")
+(setq comment-style 'extra-line)
 (add-hook 'c-mode-hook
           '(lambda ()
              (setq ac-sources (delq 'ac-source-yasnippet ac-sources))
