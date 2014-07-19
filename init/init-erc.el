bc04728855d8639eefbad0defe64d2649ca8a712
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-07-19 10:33:10 +0800

    erc-mode: set logging

--- init/init-erc.el
+++ init/init-erc.el
@@ -1,4 +1,10 @@
 ;; erc: hide join part messages
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
+(setq erc-log-channels t)
+(setq erc-log-channels-directory "~/.erc/log/")
+(setq erc-log-insert-log-on-open t)
+(setq erc-hide-timestamps nil)
+(setq erc-log-file-coding-system 'utf-8)
+(add-hook 'erc-insert-post-hook 'erc-save-buffer-in-logs)
 
 (provide 'init-erc)
