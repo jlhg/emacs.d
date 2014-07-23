940f7c7454c3622ff4ec5ef3fa06bc4ddc62d21a
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-07-23 18:16:48 +0800

    Add tern mode for Javascript

--- init/init-js-mode.el
+++ init/init-js-mode.el
@@ -1,11 +1,25 @@
 ;; js2-mode
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(setq js2-basic-offset 2)
+
+(eval-after-load "js2-mode"
+  '(progn
+     (setq js2-missing-semi-one-line-override t)
+     (setq js2-strict-missing-semi-warning nil)
+     (setq-default js2-basic-offset 2)))
 
 (defun json-format ()
   (interactive)
   (save-excursion
     (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)))
+
+;; tern-mode
+(add-to-list 'load-path "~/.emacs.d/package/tern")
+(autoload 'tern-mode "tern.el" nil t)
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
 
 (provide 'init-js-mode)
