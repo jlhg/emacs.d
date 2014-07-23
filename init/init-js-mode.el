51483ccef2c5e008a43a0418abfead1af4eb9eeb
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-07-24 03:25:06 +0800

    js2-mode: add skewer-mode and its dependencies; set shortcut for tern-ac-complete

--- init/init-js-mode.el
+++ init/init-js-mode.el
@@ -1,6 +1,7 @@
 ;; js2-mode
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
 (eval-after-load "js2-mode"
   '(progn
@@ -17,15 +18,32 @@
 (add-to-list 'load-path "~/.emacs.d/package/tern")
 (autoload 'tern-mode "tern.el" nil t)
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(defun my-tern-ac-complete ()
+  "Complete code at point by tern."
+  (interactive)
+  (tern-ac-complete-request
+   (lambda ()
+     (let ((ac-sources (cons 'ac-source-tern-completion ac-sources)))
+       (auto-complete)))))
 (eval-after-load 'tern
   '(progn
      (require 'tern-auto-complete)
+     (setq tern-ac-on-dot nil)
+     (define-key tern-mode-keymap (kbd "TAB") 'my-tern-ac-complete)
      (tern-ac-setup)))
 
 ;; jshint-mode
 (add-to-list 'load-path "~/.emacs.d/package/jshint-mode")
 (require 'flymake-jshint)
 (add-hook 'js2-mode-hook
-     (lambda () (flymake-mode t)))
+          (lambda () (flymake-mode t)))
+
+;; skewer-mode
+(add-to-list 'load-path "~/.emacs.d/package/emacs-web-server")
+(add-to-list 'load-path "~/.emacs.d/package/skewer-mode")
+(require 'skewer-mode)
+(add-hook 'js2-mode-hook 'skewer-mode)
+(add-hook 'css-mode-hook 'skewer-css-mode)
+(add-hook 'html-mode-hook 'skewer-html-mode)
 
 (provide 'init-js-mode)
