e352715a53de0cdb1dbb7e93ddecac7e07d81515
Jian-Long Huang <jlhgln@gmail.com>
2015-07-13 10:25:35 +0800

    Deprecate auto-java-complete
    An error occurred during auto-complete in java-mode:
    
    Error running timer `ac-update-greedy': (wrong-type-argument stringp
    nil)
    
    I don't know why, so remove it.

--- init/init-java-mode.el
+++ init/init-java-mode.el
@@ -1,7 +1,3 @@
-(require 'ajc-java-complete)
-(require 'ajc-java-complete-config)
-(add-hook 'java-mode-hook 'ajc-java-complete-mode)
-(add-hook 'find-file-hook 'ajc-4-jsp-find-file-hook)
 (add-hook 'java-mode-hook
           '(lambda ()
              (flymake-mode t)
