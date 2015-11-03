24a0d50a532ded9a1949c945082d14d95b54b6ff
Jian-Long Huang <jlhgln@gmail.com>
2015-11-03 17:05:03 +0800

    Configure org-mode

--- init/init-org-mode.el
+++ init/init-org-mode.el
@@ -6,4 +6,18 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
+(setq org-agenda-files '("~/.emacs.d/org-notes"))
+
+;; Add the date to the CLOSED tag
+(setq org-log-done 'time)
+
+(setq org-todo-keywords
+      '((sequence "TODO" "|" "DONE" "CANCELED")))
+
+(setq org-todo-keywords-for-agenda
+      '("TODO" "DONE" "CANCELED"))
+
+(setq org-todo-keyword-faces
+      '(("CANCELED" . (:foreground "blue" :weight bold))))
+
 (provide 'init-org-mode)
