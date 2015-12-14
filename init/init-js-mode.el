7c65d73a531f09e807892fe615a8c200a71f3c80
Jian-Long Huang <jlhgln@gmail.com>
2015-12-14 11:40:18 +0800

    Add js-doc.el

--- init/init-js-mode.el
+++ init/init-js-mode.el
@@ -42,4 +42,14 @@
 ;; nodejs-repl
 (require 'nodejs-repl)
 
+;; js-doc
+;; https://github.com/mooz/js-doc
+(add-to-list 'load-path "~/.emacs.d/package/js-doc")
+(require 'js-doc)
+
+(add-hook 'js2-mode-hook
+          '(lambda ()
+             (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
+             (define-key js2-mode-map "@" 'js-doc-insert-tag)))
+
 (provide 'init-js-mode)
