b4c46697c1f7678c81579668717b7f1706546f57
Jian-Long Huang <jlhgln@gmail.com>
2016-08-19 01:56:15 +0800

    erc: Change the log directory to be inside .emacs.d

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ dash_dir = ${pkg_dir}/dash.el
 
 all:
 	emacs --batch --eval '(byte-recompile-directory ".")'
-	mkdir -p backup
+	mkdir -p backup erc/log
 	chmod 700 backup
 	cd ${magit_dir} && echo "LOAD_PATH = -L ${magit_dir}/lisp -L ${dash_dir}" >config.mk && make
--- init/init-erc.el
+++ init/init-erc.el
@@ -1,7 +1,7 @@
 ;; erc: hide join part messages
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 (setq erc-log-channels t)
-(setq erc-log-channels-directory "~/.erc/log/")
+(setq erc-log-channels-directory "~/.emacs.d/erc/log/")
 (setq erc-log-insert-log-on-open t)
 (setq erc-hide-timestamps nil)
 (setq erc-log-file-coding-system 'utf-8)
