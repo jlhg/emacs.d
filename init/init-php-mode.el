1de3d804bb0dad88de6a1de2181f5a0f3d53cc93
Jian-Long Huang <huang@jianlong.org>
2016-10-24 01:46:47 +0800

    Upgrade php-mode to the latest version which supports php7

--- init/init-php-mode.el
+++ init/init-php-mode.el
@@ -1,6 +1,12 @@
+(add-to-list 'load-path "~/.emacs.d/package/php-mode")
+(add-to-list 'load-path "~/.emacs.d/package/php-mode/skeleton")
+
 (require 'php-mode)
 (add-hook 'php-mode-hook
           (lambda ()
             (subword-mode 1)))
+
+(eval-after-load 'php-mode
+  '(require 'php-ext))
 
 (provide 'init-php-mode)
