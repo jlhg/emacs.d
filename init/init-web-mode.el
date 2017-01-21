2abdf7585d54f9f0b27883ca7aed09f9a3d9c2a1
Jian-Long Huang <huang@jianlong.org>
2017-01-21 10:28:36 +0800

    web-mode: Support .blade.php file

--- init/init-web-mode.el
+++ init/init-web-mode.el
@@ -2,6 +2,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
