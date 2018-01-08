51194444a925decdecc5f67f66ec0905e139264a
Jian-Long Huang <huang@jianlong.org>
2018-01-08 16:56:23 +0800

    Still want to use indent-guide

--- init/init-indent-guide.el
+++ init/init-indent-guide.el
@@ -1,21 +1,21 @@
-;; (require 'indent-guide)
+(require 'indent-guide)
 
-;; ;; If you want to enable indent-guide-mode in all buffers, call function indent-guide-global-mode.
-;; (indent-guide-global-mode)
+;; If you want to enable indent-guide-mode in all buffers, call function indent-guide-global-mode.
+(indent-guide-global-mode)
 
-;; ;; To show not only one guide line but all guide lines recursively, set “indent-guide-recursive” non-nil.
-;; (setq indent-guide-recursive nil)
+;; To show not only one guide line but all guide lines recursively, set “indent-guide-recursive” non-nil.
+(setq indent-guide-recursive nil)
 
 ;; Config for Highlight-Indentation-for-Emacs
-(add-to-list 'load-path "~/.emacs.d/package/Highlight-Indentation-for-Emacs")
-(require 'highlight-indentation)
+;; (add-to-list 'load-path "~/.emacs.d/package/Highlight-Indentation-for-Emacs")
+;; (require 'highlight-indentation)
 
-(define-globalized-minor-mode global-highlight-indentation-mode highlight-indentation-mode
-  (lambda ()
-    ;; (highlight-indentation-mode 1)
-    (highlight-indentation-current-column-mode 1)
-    ))
+;; (define-globalized-minor-mode global-highlight-indentation-mode highlight-indentation-mode
+;;   (lambda ()
+;;     ;; (highlight-indentation-mode 1)
+;;     (highlight-indentation-current-column-mode 1)
+;;     ))
 
-(global-highlight-indentation-mode 1)
+;; (global-highlight-indentation-mode 1)
 
 (provide 'init-indent-guide)
