4b565dd31144c97490c2964b1a4fd9d1cb381c92
Jian-Long Huang <huang@jianlong.org>
2020-02-28 14:56:49 +0800

    go-mode: treat underscore as subword

--- init/init-go-mode.el
+++ init/init-go-mode.el
@@ -2,6 +2,30 @@
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 
+(setq go-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?+  "." st)
+    (modify-syntax-entry ?-  "." st)
+    (modify-syntax-entry ?%  "." st)
+    (modify-syntax-entry ?&  "." st)
+    (modify-syntax-entry ?|  "." st)
+    (modify-syntax-entry ?^  "." st)
+    (modify-syntax-entry ?!  "." st)
+    (modify-syntax-entry ?=  "." st)
+    (modify-syntax-entry ?<  "." st)
+    (modify-syntax-entry ?>  "." st)
+    (modify-syntax-entry ?/  ". 124b" st)
+    (modify-syntax-entry ?*  ". 23" st)
+    (modify-syntax-entry ?\n "> b" st)
+    (modify-syntax-entry ?\" "\"" st)
+    (modify-syntax-entry ?\' "\"" st)
+    (modify-syntax-entry ?`  "\"" st)
+    (modify-syntax-entry ?\\ "\\" st)
+    ;; TODO make _ a symbol constituent now that xemacs is gone
+    ;; (modify-syntax-entry ?_  "w" st)
+
+    st))
+
 (require 'golint)
 
 (provide 'init-go-mode)
