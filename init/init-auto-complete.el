43e49e78fd4881fa4f7781996e553c38bd67ecb8
Jian-Long Huang <contact@jianlong.org>
2021-10-04 02:09:32 +0800

    Revert "auto-complete: Add stop words"
    This reverts commit 73d0b77daee81c5dc20e21c117a57ab7cbb30377.

--- init/init-auto-complete.el
+++ init/init-auto-complete.el
@@ -17,10 +17,6 @@
 (define-key ac-completing-map (kbd "<down>") nil)
 (define-key ac-completing-map (kbd "RET") nil)
 (define-key ac-completing-map (kbd "<return>") nil)
-(add-to-list 'ac-stop-words "{")
-(add-to-list 'ac-stop-words "[")
-(add-to-list 'ac-stop-words "(")
-(add-to-list 'ac-stop-words "<")
 
 ;; ac/yas color
 (defface ac-yasnippet-candidate-face
