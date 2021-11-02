d421a7676b2a0503283aa4cfb486775158e2c61a
Jian-Long Huang <contact@jianlong.org>
2021-11-02 23:49:36 +0800

    Update init-native-comp

--- init/init-native-comp.el
+++ init/init-native-comp.el
@@ -1,6 +1,11 @@
 (when (fboundp 'native-compile-async)
-  (if (yes-or-no-p "async compile?")
-      (setq comp-deferred-compilation t)
-    (setq comp-deferred-compilation nil)))
+  (if (yes-or-no-p "async compile? ")
+      (progn
+        (setq package-native-compile t)
+        (native-compile-async "~/.emacs.d/package" 'recursively)
+        ;; Block until native compilation has finished.
+        (while (or comp-files-queue
+                   (> (comp-async-runnings) 0))
+          (sleep-for 1)))))
 
 (provide 'init-native-comp)
