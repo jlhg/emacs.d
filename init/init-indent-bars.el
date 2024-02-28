adf2b6b838cc1a06ac114a0e7540b855dd473ad0
Jian-Long Huang <contact@jlhg.org>
2024-02-29 03:10:21 +0800

    indent-bars: Set indent-bars-display-on-blank-lines to nil for better performance

--- init/init-indent-bars.el
+++ init/init-indent-bars.el
@@ -3,6 +3,7 @@
 (require 'indent-bars)
 
 (setq indent-bars-color '(highlight :face-bg t :blend 0))
+(setq indent-bars-display-on-blank-lines nil)
 
 (add-hook 'prog-mode-hook 'indent-bars-mode)
 (add-hook 'yaml-mode-hook 'indent-bars-mode)
