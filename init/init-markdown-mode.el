94a9426b369a910ca65ee40082b35803c99a36bb
Jian-Long Huang <contact@jlhuang.com>
2024-04-17 00:53:32 +0800

    init-markdown-mode.el: Setup fill-mode

--- init/init-markdown-mode.el
+++ init/init-markdown-mode.el
@@ -5,13 +5,8 @@
       (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (add-hook  'markdown-mode-hook
            (lambda ()
-             (setq indent-tabs-mode t)
-             (setq auto-fill-mode -1)
-             (setq visual-line-mode t)
-             (setq visual-fill-column-mode t)
-             (setq fill-column 120)
-             (setq markdown-command "markdown")
-             (when (require 'auto-complete nil t) ; no error whatever auto-complete.el is not installed.
-               (auto-complete-mode t))))
+             (auto-fill-mode t)
+             (setq fill-column 80)
+             (setq indent-tabs-mode t)))
 
 (provide 'init-markdown-mode)
