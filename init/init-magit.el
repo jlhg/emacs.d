09c442434d305349e70fe60ddef5ebb95f2f5160
Jian-Long Huang <contact@jlhuang.com>
2024-04-17 00:58:01 +0800

    init-magit.el: Make magit-status always open in current window

--- init/init-magit.el
+++ init/init-magit.el
@@ -17,6 +17,24 @@
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;; Make magit-status always open in current window.
+;; https://github.com/magit/magit/issues/2541#issuecomment-195420321
+(setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer
+         buffer
+         (cond ((and (derived-mode-p 'magit-mode)
+                     (eq (with-current-buffer buffer major-mode)
+                         'magit-status-mode))
+                nil)
+               ((memq (with-current-buffer buffer major-mode)
+                      '(magit-process-mode
+                        magit-revision-mode
+                        magit-diff-mode
+                        magit-stash-mode))
+                nil)
+               (t
+                '(display-buffer-same-window))))))
 (setq magit-section-visibility-indicator nil)
 
 ;; https://www.reddit.com/r/emacs/comments/bdsfb7/comment/el0lowt/?utm_source=share&utm_medium=web2x&context=3
