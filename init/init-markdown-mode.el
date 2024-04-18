8f5ebe0f1136abead1607527d849b7e7da313b0c
Jian-Long Huang <contact@jlhuang.com>
2024-04-19 01:39:24 +0800

    init-markdown-mode.el: Set fill-column to 200; upgrade markdown-mode.el

--- init/init-markdown-mode.el
+++ init/init-markdown-mode.el
@@ -1,16 +1,16 @@
 (require 'visual-fill-column)
 
-(autoload 'markdown-mode "markdown-mode.el"
-  "Major mode for editing Markdown files" t)
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . markdown-mode))
 
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (add-hook  'markdown-mode-hook
            (lambda ()
              (auto-fill-mode -1)
              (visual-line-mode nil)
              (visual-line-fill-column-mode t)
-             (setq fill-column 80)
+             (setq fill-column 200)
              (setq indent-tabs-mode t)))
 
 (provide 'init-markdown-mode)
