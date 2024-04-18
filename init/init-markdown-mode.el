b6034d89ad63a7731c907fd22e5b0d6bd12e3de1
Jian-Long Huang <contact@jlhuang.com>
2024-04-19 01:15:47 +0800

    markdown-mode: Enable visual-line-fill-column-mode

--- init/init-markdown-mode.el
+++ init/init-markdown-mode.el
@@ -1,3 +1,5 @@
+(require 'visual-fill-column)
+
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
 
@@ -5,7 +7,9 @@
       (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (add-hook  'markdown-mode-hook
            (lambda ()
-             (auto-fill-mode t)
+             (auto-fill-mode -1)
+             (visual-line-mode nil)
+             (visual-line-fill-column-mode t)
              (setq fill-column 80)
              (setq indent-tabs-mode t)))
 
