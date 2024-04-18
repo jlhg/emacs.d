87b7a825c7d67c5770bbe0998ab7b00eb595d45d
Jian-Long Huang <contact@jlhuang.com>
2024-04-19 01:43:10 +0800

    init-markdown-mode.el: Set fill-column to 120

--- init/init-markdown-mode.el
+++ init/init-markdown-mode.el
@@ -10,7 +10,7 @@
              (auto-fill-mode -1)
              (visual-line-mode nil)
              (visual-line-fill-column-mode t)
-             (setq fill-column 200)
+             (setq fill-column 120)
              (setq indent-tabs-mode t)))
 
 (provide 'init-markdown-mode)
