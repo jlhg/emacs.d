2214cf010ec78073739b78c5da1a3374fd817e15
Jian-Long Huang <contact@jlhuang.com>
2024-08-25 00:44:31 +0800

    init-helm: Fix helm-config.el not found error

--- init/init-helm.el
+++ init/init-helm.el
@@ -1,7 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/package/emacs-async")
 (add-to-list 'load-path "~/.emacs.d/package/helm")
 
-(require 'helm-config)
+(require 'helm)
+(require 'helm-autoloads)
 
 ;; This program makes your CSS/SCSS/LESS coding faster
 ;; and easier than ever.
