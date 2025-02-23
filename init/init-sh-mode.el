077b91d77296083ff242e866a4ef3bfbc2dc1403
Jian-Long Huang <contact@jlhuang.com>
2025-02-24 06:38:37 +0800

    sh-mode: Add env to auto-mode-alist

--- init/init-sh-mode.el
+++ init/init-sh-mode.el
@@ -1,6 +1,6 @@
 (setq-default sh-basic-offset 2)
 (setq-default sh-indentation 2)
 (add-to-list 'auto-mode-alist
-             '("\\.?\\(bashrc\\|bash_profile\\|profile\\)\\'"  . sh-mode))
+             '("\\.?\\(bashrc\\|bash_profile\\|profile\\|env\\)\\'"  . sh-mode))
 
 (provide 'init-sh-mode)
