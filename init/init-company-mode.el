786b891bb639b7619fa27f3972134f148fcca0a8
Jian-Long Huang <contact@jlhuang.com>
2025-06-19 05:39:56 +0800

    company-mode: Use M-n/M-p to navigate candidate items.

--- init/init-company-mode.el
+++ init/init-company-mode.el
@@ -10,12 +10,19 @@
 (setq company-idle-delay 0)
 
 ;; Number the candidates (use M-1, M-2 etc to select completions).
-(setq company-show-numbers t)
+(setq company-show-quick-access t)
 
 ;; The minimum prefix length for idle completion.
 (setq company-minimum-prefix-length 1)
 
 ;; Disable icons
 (setq company-format-margin-function nil)
+
+;; When the candidate window is active, use M-n/M-p to navigate items.
+(with-eval-after-load 'company
+  (keymap-unset company-active-map "C-n" 'remove)
+  (keymap-unset company-active-map "C-p" 'remove)
+  (define-key company-active-map (kbd "M-n") #'company-select-next)
+  (define-key company-active-map (kbd "M-p") #'company-select-previous))
 
 (provide 'init-company-mode)
