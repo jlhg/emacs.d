4c0c9ffb8ffef9e56dd9f58af9dfcc298bb8c0db
Jian-Long Huang <contact@jlhuang.com>
2025-07-08 09:22:01 +0800

    feat(vertico): add ido-like directory navigation
    - Require vertico-directory for Ido-style navigation.
    - Bind RET to enter directories.
    - Bind DEL and M-DEL for character and word deletion within the minibuffer.

--- init/init-vertico.el
+++ init/init-vertico.el
@@ -10,6 +10,13 @@
 (require 'vertico-grid)
 (vertico-grid-mode 1)
 
+;; Ido-like directory navigation for Vertico
+(require 'vertico-directory)
+(with-eval-after-load 'vertico
+  (keymap-set vertico-map "RET" #'vertico-directory-enter)
+  (keymap-set vertico-map "DEL" #'vertico-directory-delete-char)
+  (keymap-set vertico-map "M-DEL" #'vertico-directory-delete-word))
+
 (require 'orderless)
 (setq completion-styles '(orderless basic)
       completion-category-overrides '((file (styles basic partial-completion))))
